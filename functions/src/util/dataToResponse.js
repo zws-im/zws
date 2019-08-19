8bcf77e6da255929eb7f1d4bbbe11d9267a9d987
Jonah Snider <me@jonahsnider.ninja>
2019-08-18 19:20:56 -0700

    refactor: rewrite how stats result is built
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- functions/src/util/dataToResponse.js
+++ functions/src/util/dataToResponse.js
@@ -1,12 +1,31 @@
+const mergeDefault = require("./mergeDefault");
+
 /**
  * Helper function for URL stats.
  * @param {Object} data
  */
-module.exports = data => ({
-  shorten: data.stats.shorten,
-  get: data.stats.get,
-  usage: {
-    get: (data.usage && data.usage.get ? data.usage.get : []).map(firestoreTimestamp => firestoreTimestamp.toMillis()),
-    shorten: (data.usage && data.usage.shorten ? data.usage.shorten : []).map(firestoreTimestamp => firestoreTimestamp.toMillis())
-  }
-});
+module.exports = data => {
+  mergeDefault({
+    stats: {
+      shorten: 0,
+      get: 0
+    },
+    usage: {
+      shorten: [],
+      get: []
+    }
+  }, data);
+
+  return {
+    shorten: data.stats.shorten,
+    get: data.stats.get,
+    usage: {
+      get: data.usage.get.map(firestoreTimestamp =>
+        firestoreTimestamp.toMillis()
+      ),
+      shorten: data.usage.shorten.map(firestoreTimestamp =>
+        firestoreTimestamp.toMillis()
+      )
+    }
+  };
+};
