70fe485399b678bd6e48454a3aa3d120a1e2571f
Jonah Snider <jonah@jonah.pw>
2021-02-21 05:34:50 -0800

    feat(logger): disable colors if not supported

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,6 @@
 import {Logger} from 'tslog';
+import supportsColor from 'supports-color';
 
-const logger = new Logger({displayFilePath: 'hidden', displayFunctionName: false});
+const logger = new Logger({displayFilePath: 'hidden', displayFunctionName: false, colorizePrettyLogs: supportsColor.stdout && supportsColor.stderr});
 
 export default logger;
