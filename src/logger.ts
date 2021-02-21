a8cfd48a3b76356b11321c166cb9db1fa5505f9f
Jonah Snider <jonah@jonah.pw>
2021-02-21 08:10:54 -0800

    feat: stop showing timestamps in logs on Heroku

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,13 @@
-import {Logger} from 'tslog';
 import supportsColor from 'supports-color';
+import {Logger} from 'tslog';
+import {heroku} from './config/env';
 
-const logger = new Logger({displayFilePath: 'hidden', displayFunctionName: false, colorizePrettyLogs: supportsColor.stdout && supportsColor.stderr});
+const logger = new Logger({
+	displayFilePath: 'hidden',
+	displayFunctionName: false,
+	colorizePrettyLogs: supportsColor.stdout && supportsColor.stderr,
+	// Heroku logs display timestamps next to each line
+	displayDateTime: !heroku
+});
 
 export default logger;
