f79038749a56a28f6311ac1da7c7389c74809e46
Jonah Snider <jonah@jonah.pw>
2021-02-22 10:54:34 -0800

    feat(server): stop adding CORS headers

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,5 @@
 import convert from 'convert';
 import createServer, {RouteOptions} from 'fastify';
-import cors from 'fastify-cors';
 import {heroku} from '../config/env';
 import {fastifyLogger} from '../logger';
 import registerHooks from './hooks';
@@ -17,8 +16,5 @@ registerHooks(fastify);
 for (const route of Object.values(routes)) {
 	fastify.route(route as RouteOptions);
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-fastify.register(cors).then(() => {}, fastifyLogger.error);
 
 export default fastify;
