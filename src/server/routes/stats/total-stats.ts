135c5019b148b7906246258fd7228a9d1673bba7
Jonah Snider <jonah@jonah.pw>
2021-02-22 20:48:19 -0800

    feat(stats): support pretty-printing counts

--- src/server/routes/stats/total-stats.ts
+++ src/server/routes/stats/total-stats.ts
@@ -1,12 +1,29 @@
 import {FastifyInstance, RawReplyDefaultExpression, RawRequestDefaultExpression, RawServerDefault, RouteOptions} from 'fastify';
 import {urls} from '../../../services';
+import S from 'fluent-json-schema';
 
+interface Stats<T extends number | string> {
+	urls: T;
+	visits: T;
+}
 export default function declareRoute(fastify: FastifyInstance) {
-	const route: RouteOptions<RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression, {Reply: {urls: number; visits: number}}> = {
+	const route: RouteOptions<
+		RawServerDefault,
+		RawRequestDefaultExpression,
+		RawReplyDefaultExpression,
+		{Querystring: {format: boolean}; Reply: Stats<number> | Stats<string>}
+	> = {
 		method: 'GET',
 		url: '/stats',
+		schema: {
+			querystring: S.object().prop('format', S.boolean().default(false))
+		},
 		handler: async (request, reply) => {
 			const stats = await urls.totalStats();
+
+			if (request.query.format) {
+				return {urls: stats.urls.toLocaleString(), visits: stats.visits.toLocaleString()};
+			}
 
 			return stats;
 		}
