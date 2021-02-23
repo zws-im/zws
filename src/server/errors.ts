33bf06e77ef17a53d7ae0cd726ccad1d6641cd83
Jonah Snider <jonah@jonah.pw>
2021-02-22 17:41:48 -0800

    docs: add comment for UniqueShortIdTimeout error

--- src/server/errors.ts
+++ src/server/errors.ts
@@ -6,7 +6,8 @@ export const UrlNotFound = createError('E_URL_NOT_FOUND', 'Shortened URL not fou
 /** When a user attempted to shorten a URL that had the same hostname as the app does. */
 export const AttemptedShortenHostname = createError('E_SHORTEN_HOSTNAME', 'Shortening a URL with the same hostname as the server is disallowed', 422);
 
-export const UniqueShortIdTimeout = createError('E_UNIQUE_SHORT_ID_TIMEOUT', `Couldn't generate a new shortened ID in %s attempts`, 503);
+/** The maximum number of attempts to generate a unique short ID were hit. */
+export const UniqueShortIdTimeout = createError('E_UNIQUE_SHORT_ID_TIMEOUT', `Couldn't generate a unique shortened ID in %s attempts`, 503);
 
 /** An incorrect API key was provided. */
 export const IncorrectApiKey = createError('E_INCORRECT_API_KEY', 'The provided API key was not correct', 401);
