0183496a609435f41abbbff240c6cca8dd8d323d
Jonah Snider <jonah@jonahsnider.com>
2021-05-30 14:58:37 -0700

    feat(stats): use millify to format numbers

--- src/services/format.ts
+++ src/services/format.ts
@@ -1,19 +1,5 @@
-const mappings = Object.entries({
-	B: 1e9,
-	M: 1e6,
-	k: 1e3
-});
+import millify from 'millify';
 
 export function abbreviateNumber(number: number): string {
-	for (const [unit, ratio] of mappings) {
-		if (ratio <= number) {
-			const abbreviated = number / ratio;
-
-			const withDecimal = abbreviated.toFixed(1);
-
-			return `${withDecimal.length > 3 || ratio === number ? Math.round(abbreviated) : withDecimal}${unit}`;
-		}
-	}
-
-	return number.toString();
+	return millify(number);
 }
