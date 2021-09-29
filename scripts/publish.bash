224ba10ed01671792ba28b2d18161ed7959d3ca9
Jonah Snider <jonah@jonahsnider.com>
2021-09-29 00:04:12 -0700

    ci(deploy): change path to `Dockerfile`

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -3,4 +3,4 @@
 NEXT_VERSION=$1
 
 # Build and publish
-docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64,linux/arm64,linux/arm/v7 ../Dockerfile
+docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64,linux/arm64,linux/arm/v7 .
