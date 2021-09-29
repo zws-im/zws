4aceabfa7449fa37af44d847a25e3b14a2102c60
Jonah Snider <jonah@jonahsnider.com>
2021-09-29 01:48:40 -0700

    ci(deploy): disable ARM builds

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -4,4 +4,6 @@ set -e
 NEXT_VERSION=$1
 
 # Build and publish
-docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64,linux/arm64,linux/arm/v7 .
+# TODO: Re-enable ARM builds once Prisma binaries can be installed
+# docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64,linux/arm64,linux/arm/v7 .
+docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64 .
