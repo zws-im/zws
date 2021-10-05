3a466b88e13da2c1fd5cd81c2980da60f827d2af
Jonah Snider <jonah@jonahsnider.com>
2021-10-04 23:59:00 -0700

    build(docker): stop trying to compile for `linux/arm/v7`

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -5,5 +5,5 @@ NEXT_VERSION=$1
 
 # Build and publish
 # TODO: Re-enable ARM builds once Prisma binaries can be installed
-# docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64,linux/arm64,linux/arm/v7 .
+# docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64,linux/arm64 .
 docker buildx build --pull --push --tag zwsim/zws:latest --tag zwsim/zws:$NEXT_VERSION --platform linux/amd64 .
