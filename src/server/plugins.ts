ee5dbca7535c8f185a1f3cdc27a5186fbf7f314d
Jonah Snider <jonah@jonahsnider.com>
2021-10-17 00:50:53 -0700

    fix(server): fix response bodies from auth errors

--- src/server/plugins.ts
+++ src/server/plugins.ts
@@ -1,38 +1,29 @@
 import type {FastifyInstance} from 'fastify';
-import authPlugin from 'fastify-auth';
 import bearerAuthPlugin from 'fastify-bearer-auth';
 import swaggerPlugin from 'fastify-swagger';
-import {server} from '../config/index.js';
-import {configLogger} from '../logger.js';
+import * as config from '../config/index.js';
 import {IncorrectApiKey, MissingApiKey} from './errors.js';
 import openapi from './openapi.js';
 
 export default async function registerPlugins(fastify: FastifyInstance): Promise<void> {
-	const plugins = [
-		fastify.register(swaggerPlugin, {
-			openapi,
-			routePrefix: '/docs/api',
-			exposeRoute: true,
-		}),
-	];
+	await fastify.register(swaggerPlugin, {
+		openapi,
+		routePrefix: '/docs/api',
+		exposeRoute: true,
+	});
 
 	if (config.server.apiKey !== null) {
-		plugins.push(
-			fastify.register(bearerAuthPlugin, {
-				keys: new Set([config.server.apiKey]),
-				// TODO: fastify-bearer-auth v6 makes this error response logic stop working
-				errorResponse: (error: Error) => {
-					switch (error.message) {
-						case 'missing authorization header':
-							return new MissingApiKey() as unknown as {error: string};
-						default:
-							return new IncorrectApiKey() as unknown as {error: string};
-					}
-				},
-			}),
-			fastify.register(authPlugin),
-		);
+		await fastify.register(bearerAuthPlugin, {
+			addHook: true,
+			keys: new Set([config.server.apiKey]),
+			errorResponse: (error: Error) => {
+				switch (error.message) {
+					case 'missing authorization header':
+						return {...new MissingApiKey()} as unknown as {error: string};
+					default:
+						return {...new IncorrectApiKey()} as unknown as {error: string};
+				}
+			},
+		});
 	}
-
-	await Promise.all(plugins);
 }
