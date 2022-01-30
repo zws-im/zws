d773ae5f443d6d953e9ecef1de8b7d5a899d1735
Jonah Snider <jonah@jonahsnider.com>
2022-01-30 14:29:23 -0800

    test: ignore dist folder

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -153,9 +153,7 @@ module.exports = {
 	// ],
 
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-	// testPathIgnorePatterns: [
-	//   "/node_modules/"
-	// ],
+	testPathIgnorePatterns: [/dist/].map(x => x.source),
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
 	testRegex: [/\.spec\.ts$/],
