944f5f03a032ab52bfc5d1927d5cc631c1c61d59
Jonah Snider <jonah@jonahsnider.com>
2022-01-31 22:49:18 -0800

    test: start auth module tests

--- apps/api/src/auth/auth.guard.spec.ts
+++ apps/api/src/auth/auth.guard.spec.ts
@@ -1,8 +1,17 @@
 import {AuthGuard} from './auth.guard';
 
-describe.skip('AuthGuard', () => {
-	it('should be defined', () => {
-		// @ts-expect-error Will be mocked when this test is written
-		expect(new AuthGuard()).toBeDefined();
+describe('AuthGuard', () => {
+	describe('canActivate', () => {
+		it.todo('should not require auth for routes without roles configured');
+
+		it.todo('should apply role from API key when in header');
+
+		it.todo('should throw IncorrectApiKeyException when API key in header is incorrect');
+
+		it.todo('should throw MissingApiKeyException when API key in header is missing and default role is not present');
+
+		it.todo('should set default role when API key in header is missing and default role is present');
+
+		it.todo('should throw MissingPermissionsException when API key in header is correct but role does not match');
 	});
 });
--- apps/api/src/auth/auth.service.spec.ts
+++ apps/api/src/auth/auth.service.spec.ts
@@ -2,6 +2,16 @@ import type {TestingModule} from '@nestjs/testing';
 import {Test} from '@nestjs/testing';
 import {AuthService} from './auth.service';
 
+describe('AuthService static', () => {
+	describe('hashApiKey', () => {
+		it('should use SHA-512', () => {
+			expect(AuthService.hashApiKey('hello').digest('hex')).toBe(
+				'9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043',
+			);
+		});
+	});
+});
+
 describe.skip('AuthService', () => {
 	let service: AuthService;
 
