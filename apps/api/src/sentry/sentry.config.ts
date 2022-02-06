4840772369c072ad0788f21d36a18215743444b5
Jonah Snider <jonah@jonahsnider.com>
2022-02-06 13:39:25 -0800

    refactor: make Sentry environment stricter

--- apps/api/src/sentry/sentry.config.ts
+++ apps/api/src/sentry/sentry.config.ts
@@ -1,21 +1,34 @@
 import {Injectable} from '@nestjs/common';
 import {ConfigService} from '@nestjs/config';
 import {AppConfig} from '../app-config/app.config';
+import {Env} from '../enums/env.enum';
 import type {EnvironmentVariables} from '../interfaces/config.interface';
 
 @Injectable()
 export class SentryConfig {
 	readonly sentryDsn: string | undefined;
 	readonly release: string | undefined;
-	readonly environment: string;
+	readonly environment: Env.Production | Env.Development | undefined;
 
-	constructor(private readonly configService: ConfigService<EnvironmentVariables>, appConfig: AppConfig) {
+	constructor(private readonly configService: ConfigService<EnvironmentVariables>, private readonly appConfig: AppConfig) {
 		this.sentryDsn = this.getSentryDsn();
-		this.release = appConfig.env === 'production' ? `zws-${appConfig.version}` : undefined;
-		this.environment = appConfig.env;
+		this.release = this.getRelease();
+		this.environment = this.getEnvironment();
 	}
 
 	private getSentryDsn(): string | undefined {
 		return this.configService.get('SENTRY_DSN');
+	}
+
+	private getRelease(): string | undefined {
+		if (this.appConfig.env === Env.Production) {
+			return `zws-${this.appConfig.version}`;
+		}
+	}
+
+	private getEnvironment(): Env.Production | Env.Development | undefined {
+		if (this.appConfig.env !== Env.Test) {
+			return this.appConfig.env;
+		}
 	}
 }
