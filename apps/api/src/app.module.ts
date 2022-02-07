8aa36d07b5246f019ee57da92164f04e269bfeb9
Jonah Snider <jonah@jonahsnider.com>
2022-02-06 18:15:07 -0800

    refactor: sort AppModule imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import {Module} from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import path from 'node:path';
+import {APP_FILTER} from '@nestjs/core';
 import {AppConfigModule} from './app-config/app-config.module';
 import {AuthModule} from './auth/auth.module';
 import {HttpExceptionFilter} from './filters/http-exception.filter';
@@ -12,11 +13,14 @@ import {ShieldsBadgesModule} from './shields-badges/shields-badges.module';
 import {StatsModule} from './stats/stats.module';
 import {UrlsModule} from './urls/urls.module';
 import {GoogleCloudModule} from './google-cloud/google-cloud.module';
-import {APP_FILTER} from '@nestjs/core';
 
 @Module({
 	imports: [
+		// Modules that should be run early as possible
 		SentryModule,
+		GoogleCloudModule,
+
+		// Dynamic modules
 		ConfigModule.forRoot({
 			envFilePath: [
 				/* eslint-disable unicorn/prefer-module */
@@ -27,16 +31,19 @@ import {APP_FILTER} from '@nestjs/core';
 				/* eslint-enable unicorn/prefer-module */
 			],
 		}),
+
+		// Modules with controllers
+		StatsModule,
+		ShieldsBadgesModule,
+		// UrlsModule goes last since its controller has a /:id route which is rather broad
+		UrlsModule,
+
+		// Other modules
 		AppConfigModule,
 		LoggerModule,
 		AuthModule,
 		PrismaModule,
 		HealthModule,
-		StatsModule,
-		ShieldsBadgesModule,
-		// UrlsModule goes last since its controller has a /:id route which is rather broad
-		UrlsModule,
-		GoogleCloudModule,
 	],
 	providers: [
 		HttpExceptionFilter,
