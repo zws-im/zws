c0749f82c7eae1ae19e4a5cdc4ad14b21805814f
Jonah Snider <jonah@jonahsnider.com>
2022-02-13 14:03:12 -0800

    test: add dist directory to ignored module paths

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -85,7 +85,7 @@ module.exports = {
 	// moduleNameMapper: {},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-	// modulePathIgnorePatterns: [],
+	modulePathIgnorePatterns: [/dist/].map(x => x.source),
 
 	// Activates notifications for test results
 	// notify: false,
