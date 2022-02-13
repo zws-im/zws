4684037e761e845bc705525a257c838bc5541ba8
Jonah Snider <jonah@jonahsnider.com>
2022-02-13 15:01:51 -0800

    refactor: use dependency injection for @sentry/node in SentryFilter

--- apps/api/src/sentry/sentry.filter.ts
+++ apps/api/src/sentry/sentry.filter.ts
@@ -1,12 +1,16 @@
 import type {ArgumentsHost} from '@nestjs/common';
-import {Catch} from '@nestjs/common';
+import {Catch, Inject} from '@nestjs/common';
 import {BaseExceptionFilter} from '@nestjs/core';
-import * as Sentry from '@sentry/node';
+import {SentryNode, SENTRY_PROVIDER} from './sentry.service';
 
 @Catch()
 export class SentryFilter extends BaseExceptionFilter {
+	constructor(@Inject(SENTRY_PROVIDER) private readonly Sentry: SentryNode) {
+		super();
+	}
+
 	catch(exception: unknown, host: ArgumentsHost): void {
-		Sentry.captureException(exception);
+		this.Sentry.captureException(exception);
 
 		super.catch(exception, host);
 	}
