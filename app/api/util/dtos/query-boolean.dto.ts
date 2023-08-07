94e16b00967476af9687b38dbabba3dd25bc1abb
Jonah Snider <jonah@jonahsnider.com>
2023-08-07 10:24:13 -0700

    style: format with Rome

--- app/api/util/dtos/query-boolean.dto.ts
+++ app/api/util/dtos/query-boolean.dto.ts
@@ -1,7 +1,5 @@
 import yn from 'yn';
 import { z } from 'zod';
 
-export const QueryBooleanSchema = z
-	.string()
-	.transform((raw) => yn(raw));
+export const QueryBooleanSchema = z.string().transform((raw) => yn(raw));
 export type QueryBooleanSchema = z.infer<typeof QueryBooleanSchema>;
