d96751e30128bb627bc02c40ab5243dda33157c4
Jonah Snider <jonah@jonahsnider.com>
2023-08-28 03:05:05 -0700

    style: format

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,8 +1,11 @@
 export default function Footer() {
 	return (
 		<footer className='text-center py-5'>
-			<p className="text-[#EDE7FF]">
-				ZWS is licensed under <a className="underline text-[#9B77FF]" href='https://www.apache.org/licenses/LICENSE-2.0'>Apache 2.0</a>
+			<p className='text-[#EDE7FF]'>
+				ZWS is licensed under{' '}
+				<a className='underline text-[#9B77FF]' href='https://www.apache.org/licenses/LICENSE-2.0'>
+					Apache 2.0
+				</a>
 			</p>
 		</footer>
 	);
