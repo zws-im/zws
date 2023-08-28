d31d4cac45ecd135ec8a405394c1834b986e44ff
Jonah Snider <jonah@jonahsnider.com>
2023-08-28 09:34:55 -0700

    feat: add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
+import H1 from './components/headings/h1';
+
+export default function NotFound() {
+	return (
+		<main className='flex flex-col items-center justify-center space-y-4 max-sm:mt-32 max-md:mt-48 md:mt-72'>
+			<H1>404 - Page not found</H1>
+			<Link
+				href='/'
+				className='w-28 h-full p-4 rounded transition-opacity font-bold flex justify-center items-center bg-[#4413CB] hover:opacity-90 active:opacity-80'
+			>
+				Home <ArrowRightIcon className='ml-2 h-6 w-6' />
+			</Link>
+		</main>
+	);
+}
