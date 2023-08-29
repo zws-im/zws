19c59f1c8332f57719d2f18dc2ebebd1544379e4
Jonah Snider <jonah@jonahsnider.com>
2023-08-28 22:42:27 -0700

    fix: fix Prisma client creation during hot reloads

--- app/api/_lib/prisma.ts
+++ app/api/_lib/prisma.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
