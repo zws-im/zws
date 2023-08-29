78be8e62a2b7e31d114c3eed2e8e237c9ddcdf98
Jonah Snider <jonah@jonahsnider.com>
2023-08-29 02:56:39 -0700

    fix: show wave over navbar

--- app/components/background-decorations/wave.tsx
+++ app/components/background-decorations/wave.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 import wave from './wave.svg';
 
 export default function Wave() {
-	return <Image src={wave} alt='A wave pattern' className='absolute top-0 w-screen pointer-events-none' />;
+	return <Image src={wave} alt='A wave pattern' className='absolute top-0 w-screen pointer-events-none z-50' />;
 }
