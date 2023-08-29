783ec5e83537644e0d7ff157a20a302531998589
Jonah Snider <jonah@jonahsnider.com>
2023-08-29 01:20:27 -0700

    fix: fix minimum page height

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import DotGrid from './components/background-decorations/dot-grid';
 import LightSpot from './components/background-decorations/light-spot';
 import Wave from './components/background-decorations/wave';
 import { description, siteName } from './shared-metadata';
+import clsx from 'clsx';
 
 const inter = Lato({ weight: ['400', '700'], subsets: ['latin'] });
 
@@ -29,12 +30,12 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className={clsx(inter.className, 'min-h-screen')}>
 				<Navbar />
 				<Wave />
 				<DotGrid height={1} className='max-xl:top-[550px] xl:top-[787px] left-0 max-md:invisible' />
 				<DotGrid height={2} className='top-[145px] right-0' />
-				<div className='container mx-auto px-4 max-w-screen-xl min-h-screen'>
+				<div className='container mx-auto px-4 max-w-screen-xl'>
 					<LightSpot className='transform translate-x-[-250px] translate-y-[-200px] -z-50 max-md:w-0 max-md:h-0' />
 
 					{children}
