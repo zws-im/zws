9b45f1e3392cb1cd47c3ccf83ce4f321aae78850
Jonah Snider <jonah@jonahsnider.com>
2023-08-29 02:06:00 -0700

    fix: actually fix min page height

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,16 +30,19 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={clsx(inter.className, 'min-h-screen')}>
-				<Navbar />
+			<body className={clsx(inter.className)}>
 				<Wave />
-				<DotGrid height={1} className='max-xl:top-[550px] xl:top-[787px] left-0 max-md:invisible' />
-				<DotGrid height={2} className='top-[145px] right-0' />
-				<div className='container mx-auto px-4 max-w-screen-xl'>
-					<LightSpot className='transform translate-x-[-250px] translate-y-[-200px] -z-50 max-md:w-0 max-md:h-0' />
 
-					{children}
-					<Footer />
+				<div className='min-h-screen'>
+					<Navbar />
+					<DotGrid height={1} className='max-xl:top-[550px] xl:top-[787px] left-0 max-md:invisible' />
+					<DotGrid height={2} className='top-[145px] right-0' />
+					<div className='container mx-auto px-4 max-w-screen-xl'>
+						<LightSpot className='transform translate-x-[-250px] translate-y-[-200px] -z-50 max-md:w-0 max-md:h-0' />
+
+						{children}
+						<Footer />
+					</div>
 				</div>
 			</body>
 		</html>
