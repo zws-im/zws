eaf15ee74c523d57e02aa8258a32f47d42ac291c
Jonah Snider <jonah@jonahsnider.com>
2023-08-29 06:26:21 -0700

    feat: add redirect for api.zws.im subdomain

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_SHORTENED_BASE_URL: process.env.SHORTENED_BASE_URL,
 	},
+	async rewrites() {
+		return {
+			beforeFiles: [
+				{
+					source: '/:path*',
+					destination: '/api/:path*',
+					has: [
+						{
+							type: 'host',
+							value: 'api.zws.im',
+						},
+					],
+				},
+			],
+		};
+	},
 };
 
 module.exports = nextConfig;
