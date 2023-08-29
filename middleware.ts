1d35bc25d8e1e8ce310d50ef1a568f031641e35e
Jonah Snider <jonah@jonahsnider.com>
2023-08-29 08:21:56 -0700

    feat: improve handling of api.zws.im subdomain

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(request: NextRequest): Promise<NextResponse> {
-	// GET requests can be handled like normal
-	if (request.method === 'GET') {
-		return NextResponse.next();
+	const host = request.headers.get('host');
+
+	if (host === 'api.zws.im') {
+		const url = new URL(request.url);
+
+		url.hostname = 'zws.im';
+		url.pathname = `/api${url.pathname}`;
+
+		return NextResponse.redirect(url);
 	}
 
-	// Any other requests (really we just care about POST) should be redirected to the API
-	// This is slightly inefficient, ideally we should just proxy the request to the API, but that's not really possible in an edge function
-	// The added innefficiency is okay since shortening URLs doesn't need to be super fast
-	return NextResponse.redirect(new URL('/api', request.url));
+	return NextResponse.next();
 }
-
-export const config = {
-	matcher: '/:short',
-};
--- next.config.js
+++ next.config.js
@@ -4,22 +4,6 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_SHORTENED_BASE_URL: process.env.SHORTENED_BASE_URL,
 	},
-	async rewrites() {
-		return {
-			beforeFiles: [
-				{
-					source: '/:path*',
-					destination: '/api/:path*',
-					has: [
-						{
-							type: 'host',
-							value: 'api.zws.im',
-						},
-					],
-				},
-			],
-		};
-	},
 };
 
 module.exports = nextConfig;
