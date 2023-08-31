f44b3a1edfc52ba1e26ad30d9b938cfe080ed634
Jonah Snider <jonah@jonahsnider.com>
2023-08-31 01:52:39 -0700

    feat: track clicks on the Vercel badge in footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,18 @@
+'use client';
+
 import Image from 'next/image';
 import poweredByVercel from './powered-by-vercel.svg';
+import va from '@vercel/analytics';
+import { usePlausible } from '@/app/hooks/plausible';
 
 export default function Footer() {
+	const plausible = usePlausible();
+
+	const trackVercelClick = () => {
+		va.track('Clicked Vercel badge');
+		plausible('Clicked Vercel badge');
+	};
+
 	return (
 		<footer className='text-center py-5 mt-24 flex flex-col items-center space-y-4'>
 			<p className='text-zws-purple-50'>
@@ -10,7 +21,9 @@ export default function Footer() {
 					Apache 2.0
 				</a>
 			</p>
-			<a href='https://vercel.com/?utm_source=zws-im&utm_campaign=oss'>
+
+			{/* rome-ignore lint/a11y/useValidAnchor: The click event here is not important for user interaction */}
+			<a href='https://vercel.com/?utm_source=zws-im&utm_campaign=oss' onClick={trackVercelClick}>
 				<Image src={poweredByVercel} alt='Powered by Vercel' className='h-8' />
 			</a>
 		</footer>
--- app/hooks/plausible.ts
+++ app/hooks/plausible.ts
@@ -3,6 +3,7 @@ import { usePlausible as baseUsePlausible } from 'next-plausible';
 export type PlausibleEvents = {
 	'Shorten URL': never;
 	'Check URL stats': never;
+	'Clicked Vercel badge': never;
 };
 
 export const usePlausible = () => baseUsePlausible<PlausibleEvents>();
