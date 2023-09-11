ceede9708119cd4e925d10dab83a0b9e27944294
Jonah Snider <jonah@jonahsnider.com>
2023-09-11 07:30:12 -0700

    perf: stream instance stats instead of blocking render

---
+++ app/components/about-us/stats/stats-tiles.tsx
@@ -0,0 +1,61 @@
+import { statsService } from '@/app/api/_lib/stats/stats.service';
+import convert from 'convert';
+import millify from 'millify';
+import StatsTile from './stats-tile';
+
+async function getGitHubStars(): Promise<number> {
+	const query = new URLSearchParams({ q: 'zws-im/zws' });
+	const response = await fetch(`https://api.github.com/search/repositories?${query}`, {
+		headers: {
+			accept: 'application/vnd.github+json',
+			'X-GitHub-Api-Version': '2022-11-28',
+		},
+		next: {
+			revalidate: convert(24, 'hour').to('seconds'),
+		},
+	});
+
+	if (!response.ok) {
+		return 0;
+	}
+
+	const data = (await response.json()) as {
+		items: Array<{
+			// biome-ignore lint/nursery/useNamingConvention: Can't use camelcase here
+			full_name: string;
+			// biome-ignore lint/nursery/useNamingConvention: Can't use camelcase here
+			stargazers_count: number;
+		}>;
+	};
+
+	const zws = data.items.find((item) => item.full_name === 'zws-im/zws');
+
+	return zws?.stargazers_count ?? 0;
+}
+
+type Props = {
+	githubStars: number;
+	shortenedUrls: number;
+	visits: number;
+};
+
+function StatsTilesBase({ githubStars, shortenedUrls, visits }: Props) {
+	return (
+		<>
+			<StatsTile wide={true} name='URLs shortened' value={millify(shortenedUrls)} />
+
+			<StatsTile name='Visits' value={millify(visits).toLocaleString()} />
+			<StatsTile name='GitHub stars' value={millify(githubStars)} href='https://github.com/zws-im/zws' />
+		</>
+	);
+}
+
+export async function StatsTilesActual() {
+	const [stats, stars] = await Promise.all([statsService.getInstanceStats(), getGitHubStars()]);
+
+	return <StatsTilesBase githubStars={stars} shortenedUrls={stats.urls} visits={stats.visits} />;
+}
+
+export function StatsTilesFallback() {
+	return <StatsTilesBase githubStars={1.5e3} shortenedUrls={2e5} visits={3e6} />;
+}
--- app/components/about-us/stats/stats.tsx
+++ app/components/about-us/stats/stats.tsx
@@ -1,51 +1,12 @@
-import { configService } from '@/app/api/_lib/config/config.service';
-import { statsService } from '@/app/api/_lib/stats/stats.service';
-import convert from 'convert';
-import millify from 'millify';
-import StatsTile from './stats-tile';
-
-async function getGitHubStars(): Promise<number> {
-	const query = new URLSearchParams({ q: 'zws-im/zws' });
-	const response = await fetch(`https://api.github.com/search/repositories?${query}`, {
-		headers: {
-			accept: 'application/vnd.github+json',
-			'X-GitHub-Api-Version': '2022-11-28',
-		},
-		next: {
-			revalidate: convert(24, 'hour').to('seconds'),
-		},
-	});
-
-	if (!response.ok) {
-		return 0;
-	}
-
-	const data = (await response.json()) as {
-		items: Array<{
-			// biome-ignore lint/nursery/useNamingConvention: Can't use camelcase here
-			full_name: string;
-			// biome-ignore lint/nursery/useNamingConvention: Can't use camelcase here
-			stargazers_count: number;
-		}>;
-	};
-
-	const zws = data.items.find((item) => item.full_name === 'zws-im/zws');
-
-	return zws?.stargazers_count ?? 0;
-}
+import { Suspense } from 'react';
+import { StatsTilesActual, StatsTilesFallback } from './stats-tiles';
 
 export default async function Stats() {
-	const [stats, stars] =
-		configService.nodeEnv === 'development'
-			? [{ visits: 3e6, urls: 2e5 }, 1.5e3]
-			: await Promise.all([statsService.getInstanceStats(), getGitHubStars()]);
-
 	return (
 		<div className='min-w-max grid gap-6 grid-cols-2 max-md:w-full'>
-			<StatsTile wide={true} name='URLs shortened' value={millify(stats.urls).toLocaleString()} />
-
-			<StatsTile name='Visits' value={millify(stats.visits).toLocaleString()} />
-			<StatsTile name='GitHub stars' value={millify(stars).toLocaleString()} href='https://github.com/zws-im/zws' />
+			<Suspense fallback={<StatsTilesFallback />}>
+				<StatsTilesActual />
+			</Suspense>
 		</div>
 	);
 }
