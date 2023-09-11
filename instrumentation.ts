8ea7d286ceae01c3debd17e77e38e3fd23e6db5b
Jonah Snider <jonah@jonahsnider.com>
2023-09-11 06:31:36 -0700

    fix: do not enable Highlight.io in development

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,9 @@
 export async function register() {
-	if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID) {
+	if (
+		process.env.NODE_ENV === 'development' &&
+		process.env.NEXT_RUNTIME === 'nodejs' &&
+		process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID
+	) {
 		const { registerHighlight } = await import('@highlight-run/next/server');
 
 		registerHighlight({
