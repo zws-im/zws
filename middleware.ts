e9fafd2abb7b8d24a598fcf85cdf2c155d33b1c4
Jonah Snider <jonah@jonahsnider.com>
2023-09-11 03:44:02 -0700

    fix: add CORS to API routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const CORS_REQUEST_BASE = {
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+	},
+} satisfies RequestInit;
+
 export default async function middleware(request: NextRequest): Promise<NextResponse> {
 	const host = request.headers.get('host');
 
@@ -9,7 +15,11 @@ export default async function middleware(request: NextRequest): Promise<NextResp
 		url.hostname = 'zws.im';
 		url.pathname = `/api${url.pathname}`;
 
-		return NextResponse.redirect(url);
+		return NextResponse.redirect(url, CORS_REQUEST_BASE);
+	}
+
+	if (request.nextUrl.pathname.startsWith('/api')) {
+		return NextResponse.next(CORS_REQUEST_BASE);
 	}
 
 	return NextResponse.next();
