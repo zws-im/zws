ff7327a2c1369e63a8fa64d0ced507c8db8c3aca
Jonah Snider <jonah@jonahsnider.com>
2023-09-12 04:37:31 -0700

    refactor: extract button component from not found page into dedicated component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Metadata } from 'next';
-import Link from 'next/link';
+import Button from './components/button';
 import H1 from './components/headings/h1';
 import { siteName } from './shared-metadata';
 
@@ -17,12 +17,9 @@ export default function NotFound() {
 	return (
 		<main className='flex flex-col items-center justify-center space-y-4 max-sm:mt-32 max-md:mt-48 md:mt-72'>
 			<H1>404 - Page not found</H1>
-			<Link
-				href='/'
-				className='w-28 h-full p-4 rounded transition-opacity font-bold flex justify-center items-center bg-zws-purple-500 hover:opacity-90 active:opacity-80'
-			>
+			<Button href='/'>
 				Home <ArrowRightIcon className='ml-2 h-6 w-6' />
-			</Link>
+			</Button>
 		</main>
 	);
 }
