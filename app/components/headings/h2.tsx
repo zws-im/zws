5f8e6db7f47a6ce2983af6c2dc63697e3013d89b
Jonah Snider <jonah@jonahsnider.com>
2023-09-12 03:40:45 -0700

    fix: use h2 element instead of h1 element for H2 component

--- app/components/headings/h2.tsx
+++ app/components/headings/h2.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
 
 export default function H2({ children }: PropsWithChildren) {
-	return <h1 className='text-2xl font-bold'>{children}</h1>;
+	return <h2 className='text-2xl font-bold'>{children}</h2>;
 }
