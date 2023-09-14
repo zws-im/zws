4ba937f460cedc31fdc74ee94b9d6a03fb64655c
Jonah Snider <jonah@jonahsnider.com>
2023-09-14 02:03:00 -0700

    build: update Knip to fix Next.js plugin issues

--- knip.ts
+++ knip.ts
@@ -1,22 +1,9 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-	entry: [
-		'app/**/{page,layout,error,not-found}.tsx',
-		'app/**/route.ts',
-		'instrumentation.ts',
-		'middleware.ts',
-		'app/manifest.ts',
-		'next.config.js',
-		'next-env.d.ts',
-		'scripts/*',
-	],
-	include: [],
-
-	// Next.js plugin is bugged, see https://github.com/webpro/knip/issues/235
-	next: false,
-
+	entry: ['scripts/*.ts'],
 	ignore: [
+		'next-env.d.ts',
 		//  The [TO_RESPONSE] method is required by an external library, we don't use it directly
 		'app/api/_lib/exceptions/base.exception.ts',
 	],
