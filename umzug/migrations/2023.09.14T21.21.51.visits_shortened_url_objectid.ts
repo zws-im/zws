f31b7821f7ebd570c26e51c4bcb6f8399e672442
Jonah Snider <jonah@jonahsnider.com>
2023-09-14 14:58:10 -0700

    feat: add migration to populate shortenedUrl in visits collection

---
+++ umzug/migrations/2023.09.14T21.21.51.visits_shortened_url_objectid.ts
@@ -0,0 +1,77 @@
+import convert from 'convert';
+import type { MigrationFn } from 'umzug';
+import { MigrationContext } from '../types';
+
+export const up: MigrationFn<MigrationContext> = async (params) => {
+	const { mongo } = params.context;
+	const db = mongo.db('zws');
+
+	const visits = db.collection('visits');
+
+	const timeout = convert(30, 'minutes').to('ms');
+	const cursor = visits.aggregate(
+		[
+			{
+				$match: {
+					shortenedUrl: {
+						$exists: false,
+					},
+				},
+			},
+			{
+				$lookup: {
+					from: 'shortenedUrls',
+					as: 'longUrl',
+					let: {
+						shortBase64: '$shortenedUrlBase64',
+					},
+					pipeline: [
+						{
+							$match: {
+								$expr: {
+									$eq: ['$shortBase64', '$$shortBase64'],
+								},
+							},
+						},
+						{
+							$project: {
+								_id: 1,
+							},
+						},
+					],
+				},
+			},
+			{
+				$project: {
+					shortenedUrl: {
+						$getField: {
+							field: '_id',
+							input: {
+								$first: '$longUrl',
+							},
+						},
+					},
+				},
+			},
+			{
+				$merge: {
+					into: 'visits',
+					on: '_id',
+					whenMatched: 'merge',
+					whenNotMatched: 'fail',
+				},
+			},
+		],
+		// biome-ignore lint/nursery/useNamingConvention: Can't rename this field
+		{ maxTimeMS: timeout, maxAwaitTimeMS: timeout },
+	);
+
+	await cursor.maxTimeMS(timeout).addCursorFlag('noCursorTimeout', true).toArray();
+};
+export const down: MigrationFn<MigrationContext> = async (params) => {
+	const { mongo } = params.context;
+	const db = mongo.db('zws');
+
+	const visits = db.collection('visits');
+	visits.updateMany({}, { $unset: { shortenedUrl: 1 } });
+};
