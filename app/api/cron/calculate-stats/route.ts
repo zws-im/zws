ffe48f631ebd60296f1fa4054f3d742af8b8b159
Jonah Snider <jonah@jonahsnider.com>
2023-09-15 11:28:20 -0700

    refactor: remove now unused cron job

--- app/api/cron/calculate-stats/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { exceptionRouteWrapper } from '../../_lib/exception-route-wrapper';
-import { statsService } from '../../_lib/stats/stats.service';
-
-export const GET = exceptionRouteWrapper.wrapRoute<never>(async () => {
-	await statsService.savePreciseInstanceStats();
-
-	return new NextResponse();
-});
