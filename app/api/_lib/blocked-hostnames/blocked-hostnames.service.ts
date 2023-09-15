435e99775899f0fab50b0e38d81c1ee8d2d2a338
Jonah Snider <jonah@jonahsnider.com>
2023-09-15 11:55:23 -0700

    fix: refactor blocked hostnames service to fix Redis never being populated

--- app/api/_lib/blocked-hostnames/blocked-hostnames.service.ts
+++ app/api/_lib/blocked-hostnames/blocked-hostnames.service.ts
@@ -4,6 +4,8 @@ import { ConfigService, configService } from '../config/config.service';
 import { BlockedHostnameModel } from '../mongodb/models/blocked-hostname.model';
 import { ShortenedUrl } from '../mongodb/models/shortened-url.model';
 
+type HostnameDomainNamePair = { hostname: string; domainName: string };
+
 export class BlockedHostnamesService {
 	/** The number of seconds to cache private blocked hostnames from the database for. */
 	private static readonly BLOCKED_HOSTNAMES_CACHE_DURATION = convert(30, 'min').to('s');
@@ -23,57 +25,72 @@ export class BlockedHostnamesService {
 
 	async isHostnameBlocked(hostname: string): Promise<boolean> {
 		const domainName = hostname.replace(BlockedHostnamesService.DOMAIN_NAME_REG_EXP, '$1');
+		const hostnames = { hostname, domainName };
 
-		// If the hostname is blocked from the config, return true and avoid hitting Redis
-		if (this.blockedHostnames.has(hostname) || this.blockedHostnames.has(domainName)) {
+		if (this.cacheContainsHostname(hostnames)) {
 			return true;
 		}
 
-		// Otherwise, check Redis or the DB
-		if (await this.isHostnameBlockedInDb(hostname, domainName)) {
+		const redisResult = await this.redisContainsHostnames(hostnames);
+
+		// Check Redis as a fallback
+		if (redisResult) {
 			return true;
 		}
 
+		// Check database
+		if (await this.databaseContainsHostnames(hostnames)) {
+			return true;
+		}
+
+		if (redisResult === undefined) {
+			// Redis cache is missing, so we should populate it
+			await this.populateRedisCache();
+		}
+
+		// Otherwise, the domain name isn't blocked
 		return false;
 	}
 
-	private async isHostnameBlockedInDb(hostname: string, domainName: string): Promise<boolean> {
-		const result = await this.kv.smismember(BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY, [
-			hostname,
-			domainName,
-		]);
+	private async populateRedisCache(): Promise<void> {
+		const hostnamesDocuments = await BlockedHostnameModel.find({}, { projection: { hostname: 1 } });
+		const hostnames = hostnamesDocuments.map((document) => document.hostname);
 
-		const blockedInCache = result.some((count) => count > 0);
-
-		if (!blockedInCache) {
-			const redisCacheExists = await this.kv.exists(BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY);
-
-			if (redisCacheExists) {
-				await this.refreshRedisCache();
-
-				return this.blockedHostnames.has(hostname) || this.blockedHostnames.has(domainName);
-			}
-		}
-
-		return blockedInCache;
-	}
-
-	private async refreshRedisCache(): Promise<void> {
-		await this.getBlockedHostnamesFromDatabase();
-
-		await this.kv.sadd(BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY, this.blockedHostnames);
+		await this.kv.sadd(BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY, hostnames);
 		await this.kv.expire(
 			BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY,
 			BlockedHostnamesService.BLOCKED_HOSTNAMES_CACHE_DURATION,
 		);
 	}
 
-	private async getBlockedHostnamesFromDatabase(): Promise<void> {
-		const dbBlockedHostnames = await BlockedHostnameModel.find({}, { projection: { hostname: 1 } });
+	private async databaseContainsHostnames(hostnames: HostnameDomainNamePair): Promise<boolean> {
+		return BlockedHostnameModel.exists({ hostname: { $in: [hostnames.hostname, hostnames.domainName] } });
+	}
 
-		for (const row of dbBlockedHostnames) {
-			this.blockedHostnames.add(row.hostname);
+	/** @returns Whether the hostnames were blocked in Redis, or `undefined` if they were missing. */
+	private async redisContainsHostnames(hostnames: HostnameDomainNamePair): Promise<boolean | undefined> {
+		const result = await this.kv.smismember(BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY, [
+			hostnames.hostname,
+			hostnames.domainName,
+		]);
+
+		const blockedInCache = result.some((count) => count > 0);
+
+		if (blockedInCache) {
+			return true;
 		}
+
+		const redisCacheExists = await this.kv.exists(BlockedHostnamesService.BLOCKED_HOSTNAMES_REDIS_KEY);
+
+		if (redisCacheExists) {
+			return false;
+		}
+
+		return undefined;
+	}
+
+	private cacheContainsHostname(hostnames: HostnameDomainNamePair): boolean {
+		return this.blockedHostnames.has(hostnames.hostname) || this.blockedHostnames.has(hostnames.domainName);
 	}
 }
 
