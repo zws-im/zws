0bf82e1397efdeb0a98344c335f0278d0a8d9634
Jonah Snider <jonah@jonahsnider.com>
2024-03-29 15:11:54 -0700

    ci: make format script work in CI

--- migrate.ts
+++ migrate.ts
@@ -6,12 +6,7 @@ import { Client } from 'pg';
 import pino from 'pino';
 
 const logger = pino({
-	transport:
-		process.env.NODE_ENV === 'development'
-			? {
-					target: 'pino-pretty',
-			  }
-			: undefined,
+	transport: process.env.NODE_ENV === 'development' ? { target: 'pino-pretty' } : undefined,
 });
 
 logger.info('Migration script started');
