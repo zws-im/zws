f464d3565e4e1533f59d2a7adc4fa22f93cb7fbf
Jonah Snider <jonah@jonahsnider.com>
2024-03-29 17:57:23 -0700

    refactor(api): remove unused auth module

--- apps/api/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthMiddleware } from './auth.middleware';
-import { AuthService } from './auth.service';
-
-@Module({
-	providers: [AuthService, AuthMiddleware],
-	exports: [AuthService, AuthMiddleware],
-})
-export class AuthModule {}
