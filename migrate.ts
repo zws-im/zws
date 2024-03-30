54f951ad39d4a813fc76befd0dc06b9f4d374451
Jonah Snider <jonah@jonahsnider.com>
2024-03-29 17:34:24 -0700

    build: update Biome

--- migrate.ts
+++ migrate.ts
@@ -6,7 +6,12 @@ import { Client } from 'pg';
 import pino from 'pino';
 
 const logger = pino({
-	transport: process.env.NODE_ENV === 'development' ? { target: 'pino-pretty' } : undefined,
+	transport:
+		process.env.NODE_ENV === 'development'
+			? {
+					target: 'pino-pretty',
+				}
+			: undefined,
 });
 
 logger.info('Migration script started');
