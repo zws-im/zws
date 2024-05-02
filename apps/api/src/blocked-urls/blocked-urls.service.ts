e2daa9cb772d6cde0b96017557193d98bcc3949e
Jonah Snider <jonah@jonahsnider.com>
2024-05-01 17:57:22 -0700

    feat(api): add phishing URL heuristic

--- apps/api/src/blocked-urls/blocked-urls.service.ts
+++ apps/api/src/blocked-urls/blocked-urls.service.ts
@@ -17,11 +17,56 @@ export class BlockedUrlsService {
 			return true;
 		}
 
+		const matchesCaptchaPhishHeuristic = this.matchesCaptchaPhishHeuristic(actualUrl);
+
+		if (matchesCaptchaPhishHeuristic) {
+			return true;
+		}
+
 		const results = await Promise.all([
 			this.blockedHostnamesService.isHostnameBlocked(actualUrl),
 			this.safeBrowsingService.hasThreatMatches(actualUrl),
 		]);
 
 		return results.some(Boolean);
+	}
+
+	// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: I don't want to make this a separate function
+	private matchesCaptchaPhishHeuristic(url: URL): boolean {
+		let score = 0;
+
+		if (url.host.endsWith('.xyz') || url.host.endsWith('.top') || url.host.endsWith('.online')) {
+			score++;
+		}
+
+		if (url.hostname.includes('captcha')) {
+			score++;
+		}
+
+		if (url.hostname.includes('server') || url.hostname.includes('bot')) {
+			score++;
+		}
+
+		if (url.pathname === '/verify') {
+			score++;
+		}
+
+		if (url.searchParams.has('data')) {
+			score++;
+
+			const data = url.searchParams.get('data');
+
+			if (data) {
+				try {
+					const decoded = JSON.parse(Buffer.from(data, 'base64').toString('utf8'));
+
+					if ('guildId' in decoded || 'clientId' in decoded) {
+						return true;
+					}
+				} catch {}
+			}
+		}
+
+		return score >= 3;
 	}
 }
