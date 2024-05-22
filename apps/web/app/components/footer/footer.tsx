b9bbd0ed63879ec5fe9db8cb87ac32e72af087ec
Jonah Snider <jonah@jonahsnider.com>
2024-05-22 04:12:20 -0700

    feat(web): add contact info to page footer

--- apps/web/app/components/footer/footer.tsx
+++ apps/web/app/components/footer/footer.tsx
@@ -14,11 +14,10 @@ export function Footer() {
 	};
 
 	return (
-		<footer className='mt-24 flex flex-col items-center gap-y-4 py-5 text-center'>
+		<footer className='mt-24 flex flex-col items-center gap-4 py-5 text-center'>
 			<p className='text-zws-purple-50'>
-				ZWS is licensed under{' '}
-				<a className='text-zws-purple-400 underline' href='https://www.apache.org/licenses/LICENSE-2.0'>
-					Apache 2.0
+				<a className='text-zws-purple-400 underline' href='mailto:jonah@jonahsnider.com'>
+					Contact
 				</a>
 			</p>
 
