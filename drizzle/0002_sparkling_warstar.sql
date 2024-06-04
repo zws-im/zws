a42b70b5a3266aedf306ff8d410ae557b10d059c
Jonah Snider <jonah@jonahsnider.com>
2024-06-03 21:38:00 -0700

    feat(api): remove index creation migration

--- drizzle/0002_sparkling_warstar.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX IF NOT EXISTS "urls_blocked_index" ON "urls" USING hash (blocked);--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "urls_url_index" ON "urls" USING hash (url);--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "visits_url_short_base64_index" ON "visits" USING hash (url_short_base64);--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "visits_timestamp_index" ON "visits" USING btree (timestamp);
