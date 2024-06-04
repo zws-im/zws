918a1fa2e17fa7cf2f0f54a73989dc4c9a31ba97
Jonah Snider <jonah@jonahsnider.com>
2024-06-03 20:41:57 -0700

    feat(api): split index change into two migrations

---
+++ drizzle/0001_minor_dormammu.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS "urls_blocked_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "urls_url_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "visits_url_short_base64_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "visits_timestamp_index";
