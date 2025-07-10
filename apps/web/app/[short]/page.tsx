aaa1479c9fb7c3ba4c701e7ccad7b96f6e5e9206
Jonah Snider <jonah@jonahsnider.com>
2025-07-10 10:15:13 -0700

    perf(web): optimize [short]/page.tsx

--- apps/web/app/[short]/page.tsx
+++ apps/web/app/[short]/page.tsx
@@ -1,30 +1,35 @@
-import assert from 'node:assert/strict';
 import { notFound, RedirectType, redirect } from 'next/navigation';
 import UrlBlockedPage from './url-blocked-page';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-assert(API_URL, 'Missing NEXT_PUBLIC_API_URL env var');
+if (!API_URL) {
+	throw new TypeError('Missing NEXT_PUBLIC_API_URL env var');
+}
+
+export const runtime = 'edge';
 
 export default async function UrlSubpathPage(props: {
 	params: Promise<{ short: string }>;
 	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-	const rawSearchParams = await props.searchParams;
-	const rawParams = await props.params;
-	const searchParams = new URLSearchParams(
-		Object.entries(rawSearchParams).flatMap(([key, valueOrValues]) => {
-			if (Array.isArray(valueOrValues)) {
-				return valueOrValues.map((value) => [key, value]);
-			}
+	const [rawSearchParams, rawParams] = await Promise.all([props.searchParams, props.params]);
+	const searchParams = new URLSearchParams();
 
-			if (valueOrValues === undefined) {
-				return [];
-			}
-
-			return [[key, valueOrValues]];
-		}),
-	);
+	for (const [key, valueOrValues] of Object.entries(rawSearchParams)) {
+		switch (typeof valueOrValues) {
+			case 'string':
+				searchParams.append(key, valueOrValues);
+				break;
+			case 'object':
+				for (const value of valueOrValues) {
+					searchParams.append(key, value);
+				}
+				break;
+			case 'undefined':
+				break;
+		}
+	}
 
 	const rawShort = decodeURIComponent(rawParams.short);
 
