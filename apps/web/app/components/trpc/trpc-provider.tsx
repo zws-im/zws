dd1358d0207a27497e6fe080c3a096836d8730f1
Jonah Snider <jonah@jonahsnider.com>
2025-12-21 13:43:43 -0800

    refactor: comply with lint rules
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- apps/web/app/components/trpc/trpc-provider.tsx
+++ apps/web/app/components/trpc/trpc-provider.tsx
@@ -3,6 +3,7 @@
 import type { PropsWithChildren } from 'react';
 import { trpc } from '@/app/trpc';
 
+// biome-ignore lint/style/useComponentExportOnlyModules: This is fine
 const Component = ({ children }: PropsWithChildren) => children;
 
 export const TrpcProvider = trpc.withTRPC(Component) as typeof Component;
